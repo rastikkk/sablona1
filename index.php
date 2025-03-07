5cbfc612ea2191a00502de621aacb842bce5065a
rastikkk <rastislav.kokes@student.ukf.sk>
2025-03-07 11:40:57 +0100

    pridanie pozdravu

--- index.php
+++ index.php
@@ -58,7 +58,7 @@
       <section class="container">
         <div class="row">
           <div class="col-100 text-center">
-            <?php
+          <?php
             $hour = date('H');
             if ($hour < 12) {
                 echo "<h3>Dobré ráno</h3>";
